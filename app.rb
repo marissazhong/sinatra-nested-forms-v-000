7dd7be7b85388f84bb57286fcebbe8359dfccee1
Marissa Zhong <mwz2101@columbia.edu>
2018-07-14 11:23:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,10 +4,12 @@ module FormsLab
   class App < Sinatra::Base
 
     get '/' do
-      erb :'pirates/new'
+      erb :root
     end
 
-    
+    get '/new' do
+      erb :'pirates/new'
+    end
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
